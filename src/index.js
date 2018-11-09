6c201645c097106ee3311508abb9b143af05c620
Brodey Newman <brodeynewman@gmail.com>
2018-11-08 22:41:30 -0500

    fixing edge cases

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import React from 'react';
 
+/**
+ * Checks if key code matches the current state
+ * @param {string} code - the key code
+ * @param {Array} currentCapture - the current keys in state
+ * @returns {Boolean} - true if code matches the current capture, else otherwise
+ */
 export function doesKeyCodeMatchState(code, currentCapture) {
   return code === currentCapture.join('');
 }
 
 const withBetaComponent = options => ComposedComponent => (
   class extends React.Component {
-    constructor(props) {
-      super(props);
+    state = {
+      isListening: false,
+      isToggled: Boolean(options && options.forceEnable),
+      currentCapture: [],
+    };
 
-      this.state = {
-        isListening: false,
-        isToggled: Boolean(options && options.forceEnable),
-        currentCapture: [],
-      };
-    }
-
+    /**
+     * Checks for a valid keycode on mount.
+     * If a valid keycode exists, it will enable keydown listener.
+     * Else, it will throw an error.
+     * @returns {void} -
+     */
     componentDidMount() {
+      if (options && typeof options.keyCode !== 'string') {
+        throw new Error('[react-beta-component]: options.keyCode must be a valid string');
+      }
+
       document.addEventListener('keydown', this.handleKeyPress);
     }
 
+    /**
+     * Disables keydown event from window
+     * @returns {void}
+     */
     componentWillUnmount() {
       document.removeEventListener('keydown', this.handleKeyPress);
     }
@@ -33,8 +49,20 @@ const withBetaComponent = options => ComposedComponent => (
         this.setState(({ isToggled }) => ({
           isToggled: !isToggled,
           currentCapture: [],
+          isListening: false,
         }));
+
+        return clearTimeout(this.timeout);
       }
+
+      if (currentCapture.length > options.keyCode.length) {
+        return this.setState({
+          isListening: false,
+          currentCapture: [],
+        });
+      }
+
+      return null;
     }
 
     setTimeoutAndCapture = (key) => {
@@ -49,7 +77,9 @@ const withBetaComponent = options => ComposedComponent => (
         clearTimeout(this.timeout);
       }, options.keyCodeTimeout || 500);
 
-      this.setState({ currentCapture: [...currentCapture, key] }, this.maybeEnableBetaComponent);
+      this.setState({
+        currentCapture: [...currentCapture, key],
+      }, this.maybeEnableBetaComponent);
     }
 
     enableListening = key => this.setState({
