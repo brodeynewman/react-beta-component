654c20f84b186935cbdbefa2ce9664071afd6a5b
Brodey Newman <brodeynewman@gmail.com>
2018-11-08 20:45:03 -0500

    tracking keypress

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 const withBetaComponent = options => ComposedComponent => (
   class extends React.Component {
-    state = { isToggled: true };
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        isToggled: Boolean(options && options.forceEnable),
+      };
+    }
+
+    componentDidMount() {
+      document.addEventListener('keydown', this.handleKeyPress);
+    }
+
+    handleKeyPress = ({ key }) => {
+      console.log('keypress', key);
+    };
 
     render() {
       const { isToggled } = this.state;
 
-      return isToggled ? <ComposedComponent {...this.props} /> : null;
+      return (
+        <React.Fragment onKeyPress={this.handleKeyPress}>
+          {isToggled && <ComposedComponent {...this.props} />}
+        </React.Fragment>
+      );
     }
   }
 );
